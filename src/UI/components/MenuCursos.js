621d63205a7250b5fdf052360f7f43c91b17eac1
ianmoranf <82187893+ianmoranf@users.noreply.github.com>
2022-06-26 20:22:43 -0300

    Subiendo el componente echo en la clase

---
+++ src/UI/components/MenuCursos.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Button, View, Text } from "react-native";
+
+export default function MenuCursos() {
+    return (
+        <View>
+            <Button title="Back"></Button>
+            <Button title="Home"></Button>
+            <Text>Usuarios</Text>
+            <Button title="Alta Curso"></Button>
+            <Button title="Consulta Curso"></Button>
+            <Button title="Modificar Curso"></Button>
+        </View>
+    )
+}
