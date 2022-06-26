9dbba765877b656f9c3c2b42d547a9041aed58ed
ianmoranf <82187893+ianmoranf@users.noreply.github.com>
2022-06-26 20:26:24 -0300

    Menu profesores

---
+++ src/UI/components/MenuProfesores.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Button, Text } from "react-native";
+
+export default function MenuProfesores() {
+    return (
+        <View>
+            <Button title="Back"></Button>
+            <Button title="Home"></Button>
+            <Text> Profesores</Text>
+            <Button title="Marcar Inasistencia"></Button>
+            <Button title="Alta Profesor"></Button>
+            <Button title="Consulta Profesor"></Button>
+            <Button title="Modificar Profesor"></Button>
+        </View>
+    )
+}
