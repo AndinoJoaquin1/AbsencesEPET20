d06e863980197da05a233c38b5e277ec21465de4
AndinoJoaquin1 <andino.joaquin.epet20@gmail.com>
2022-06-29 19:44:55 -0300

    agregando dropdowns

--- src/UI/FormProfesores.js
+++ src/UI/FormProfesores.js
@@ -1,7 +1,9 @@
 import React from "react";
 import {View,TextInput,Button} from "react-native"
-
+import SelectDropdown from "react-native-select-dropdown";
 export default function FormProfesores (){
+    const Cursos=["6to 3era","6to 2da","6to 1ra"]
+    const Materias=["Etica y Deontologia","Redes II","Programacion Web Dinamica"]
     return(
         <View>
             <Button title="Back"></Button>
@@ -10,6 +12,18 @@ export default function FormProfesores (){
             <TextInput placeholder="Apellido" maxLength={25}/>
             <TextInput placeholder="D.N.I." maxLength={8}/>
             <TextInput placeholder="N° de Telefono" maxLength={13}/>
+            <SelectDropdown
+            data={Cursos}
+            onSelect={(selectedItem, index)=>console.log(selectedItem,index)}
+            buttonTextAfterSelection={(selectedItem,index)=>{return selectedItem}}
+            defaultButtonText="Ingrese el curso"
+            />
+            <SelectDropdown
+            data={Materias}
+            onSelect={(selectedItem, index)=>console.log(selectedItem,index)}
+            buttonTextAfterSelection={(selectedItem,index)=>{return selectedItem}}
+            defaultButtonText="Ingrese la materia"
+            />
             <Button title="Agregar Profesor"></Button>
         </View>
     )
