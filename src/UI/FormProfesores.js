dbbf836c35f37a692a6b6aefa58173ed077ba7b1
AndinoJoaquin1 <andino.joaquin.epet20@gmail.com>
2022-06-29 18:18:09 -0300

    agregando botones

---
+++ src/UI/FormProfesores.js
@@ -0,0 +1,12 @@
+import react from "react";
+import {View,TextInput,Button} from "react-native"
+
+export default function FormProfesores (){
+    return(
+        <View>
+            <Button title="Back"></Button>
+            <Button title="Home"></Button>
+            <Button title="Agregar Profesor"></Button>
+        </View>
+    )
+}
