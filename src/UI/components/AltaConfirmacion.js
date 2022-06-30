6597ee166624062c34b55b68513a26340074202e
AndinoJoaquin1 <andino.joaquin.epet20@gmail.com>
2022-06-29 21:01:39 -0300

    agregando text

--- src/UI/components/AltaConfirmacion.js
+++ src/UI/components/AltaConfirmacion.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {View,Button,Text} from 'react-native';
 
-export default function AltaConfimracion(){
+export default function AltaConfimracion(props){
     return(
         <View>
             <Button title="Back"></Button>
             <Button title="Home"></Button>
+            <Text>Se ha agregado {sector} {item} con exito</Text>
             <Button title="Continuar"></Button>
         </View>
     );
