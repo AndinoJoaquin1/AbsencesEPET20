76c5162c9115983c9e73a3b5ef57e46f906cbb90
ianmoranf <82187893+ianmoranf@users.noreply.github.com>
2022-07-22 13:18:12 -0300

    NavigationContainer

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import { StyleSheet, View, StatusBar } from 'react-native';
 import AltaRealizado from './src/UI/components/AltaRealizado';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
+export default function App() {
+  return (
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+  );
+}
 export default function App() {
   return (
     <View style={styles.container}>
@@ -17,4 +24,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});S
