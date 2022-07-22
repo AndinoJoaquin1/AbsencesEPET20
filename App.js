b59acab4986ce4585c1f23dcb0e171d45adf483f
ianmoranf <82187893+ianmoranf@users.noreply.github.com>
2022-07-22 13:41:45 -0300

    Prueba de creacion de un navegador

--- App.js
+++ App.js
@@ -2,12 +2,43 @@ import { StyleSheet, View, StatusBar } from 'react-native';
 import AltaRealizado from './src/UI/components/AltaRealizado';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+
   return (
     <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
   );
 }
+
+
+function LoginFom() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>LoginFom</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Inicio" component={LoginFom} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
+
+
+
+
+
 export default function App() {
   return (
     <View style={styles.container}>
