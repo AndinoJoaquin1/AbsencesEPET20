0435d4babf5933d689aa800924322171234b2cbf
AndinoJoaquin1 <andino.joaquin.epet20@gmail.com>
2022-07-24 20:22:55 -0300

    Agregando navigation en LoginForm

--- src/UI/components/LoginForm.js
+++ src/UI/components/LoginForm.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { View, TextInput, Button, Text, StyleSheet} from "react-native";
 
-export default function LoginForm() {
+export default function LoginForm({navigation}) {
     return (
         <View style={styles.container}>
             <Text>Bienvenido</Text>
             <TextInput placeholder="Usuario"></TextInput>
             <TextInput placeholder="Contraseña"></TextInput>
-            <Button> 
+            <Button 
+            title="Ingresar" 
+            onPress={()=>navigation.navigate('MainMenu')}>
             </Button>
         </View>
     );
