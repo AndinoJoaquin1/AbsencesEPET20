136694f6e4564ea894cc2e2ac984fe44768c2089
AndinoJoaquin1 <andino.joaquin.epet20@gmail.com>
2022-07-26 19:00:28 -0300

    configurando stack.screens

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import LoginForm from './src/UI/components/LoginForm';
 import MainMenu from './src/UI/components/MainMenu';
 import MenuProfesores from './src/UI/components/MenuProfesores';
-//import MenuMaterias from './src/UI/components/MenuMaterias';
-//import MenuCursos from './src/UI/components/MenuCursos';
-//import FormProfesores from './src/UI/components/FormProfesores';
-//import FormUser from './src/UI/components/FormUser';
-//import AltaRealizado from './src/UI/components/AltaRealizado';
-//import ModRealizado from './src/UI/components/ModRealizado';
-//import InstConfirmacion from './src/UI/components/InstConfirmacion';
-//import InsRealizado from './src/UI/components/InstRealizado';
+import MenuMaterias from './src/UI/components/MenuMaterias';
+import MenuCursos from './src/UI/components/MenuCursos';
+import FormProfesores from './src/UI/components/FormProfesores';
+import FormUser from './src/UI/components/FormUser';
+import AltaRealizado from './src/UI/components/AltaRealizado';
+import ModRealizado from './src/UI/components/ModRealizado';
+import InstConfirmacion from './src/UI/components/InstConfirmacion';
+import InsRealizado from './src/UI/components/InstRealizado';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -24,6 +24,14 @@ export default function App() {
         <Stack.Screen name="Inicio" component={LoginForm}/>
         <Stack.Screen name="MainMenu" component={MainMenu}/>
         <Stack.Screen name="MenuProfesores" component={MenuProfesores}/>
+        <Stack.Screen name="MenuMaterias" component={MenuMaterias}/>
+        <Stack.Screen name="MenuCursos" component={MenuCursos}/>
+        <Stack.Screen name="FormProfesores" component={FormProfesores}/>
+        <Stack.Screen name="FormUser" component={FormUser}/>
+        <Stack.Screen name="AltaRealizado" component={AltaRealizado}/>
+        <Stack.Screen name="ModRealizado" component={ModRealizado}/>
+        <Stack.Screen name="InstConfirmacion" component={InstConfirmacion}/>
+        <Stack.Screen name="InsRealizado" component={InsRealizado}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
