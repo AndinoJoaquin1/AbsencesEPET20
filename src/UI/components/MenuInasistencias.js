e01e8f96043c5a84195d44cc0f57146fc6b5fe1f
Matias Bravo <matiasbravoepet20@gmail.com>
2022-08-02 23:52:25 -0300

    feat MenuInasistencias agregando view y botón

--- src/UI/components/MenuInasistencias.js
+++ src/UI/components/MenuInasistencias.js
@@ -4,4 +4,13 @@ import {View,Button,Text,StyleSheet} from 'react-native';
 
 //menu de seleccionamiento de profesor para marcar la inasistencia
 
+export default function MenuInasistencias(){
+    return(
+        <View style={styles.container}>
+            <Button title="Back"
+            onPress={() => navigation.goBack(null)}>
+            </Button>
 
+
+            </View>
+    );
