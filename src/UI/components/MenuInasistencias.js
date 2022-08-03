632d8219a96c6be49484d81efa12b987867a8d2f
Matias Bravo <matiasbravoepet20@gmail.com>
2022-08-02 23:57:53 -0300

    feat MenuInasistencias agregando botón Profe3

--- src/UI/components/MenuInasistencias.js
+++ src/UI/components/MenuInasistencias.js
@@ -19,6 +19,11 @@ export default function MenuInasistencias(){
             onPress={() => navigation.goBack(null)}>
             </Button>
 
+
+            <Button title="Profe3"
+            onPress={() => navigation.goBack(null)}>
+            </Button>
+            
             </View>
     );
  } 
