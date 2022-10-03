851b01e8d367e171d976c796601d2ab5661f816d
Joaquin Andino <83555758+AndinoJoaquin1@users.noreply.github.com>
2022-10-03 17:08:56 -0300

    refactor: App/Navigation - migrando navegacion

--- App.js
+++ App.js
@@ -1,52 +1,15 @@
 import React from 'react';
-import LoginForm from './src/UI/components/LoginForm';
-import MainMenu from './src/UI/components/MainMenu';
-import MenuProfesores from './src/UI/components/MenuProfesores';
-import MenuMaterias from './src/UI/components/MenuMaterias';
-import MenuCursos from './src/UI/components/MenuCursos';
-import FormProfesores from './src/UI/components/FormProfesores';
-import FormUser from './src/UI/components/FormUser';
-import FormMateria from './src/UI/components/FormMateria';
-import AltaRealizado from './src/UI/components/AltaRealizado';
-import ModRealizado from './src/UI/components/ModRealizado';
-import InstConfirmacion from './src/UI/components/InstConfirmacion';
-import InsRealizado from './src/UI/components/InstRealizado';
-import MenuUsuarios from './src/UI/components/MenuUsuarios';
-import MenuInasistencias from './src/UI/components/MenuInasistencias';
-import FormCurso from './src/UI/components/FormCurso';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeBaseProvider } from 'native-base';
+import Navigation from './src/UI/nav/Navigation';
 
 
 //Ejecucion de componentes y navegacion  entre  pantallas
 
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NativeBaseProvider>
-
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Inicio">
-        <Stack.Screen name="Inicio" component={LoginForm}/>
-        <Stack.Screen name="MainMenu" component={MainMenu}/>
-        <Stack.Screen name="MenuProfesores" component={MenuProfesores}/>
-        <Stack.Screen name="MenuMaterias" component={MenuMaterias}/>
-        <Stack.Screen name="MenuCursos" component={MenuCursos}/>
-        <Stack.Screen name="MenuUsuarios" component={MenuUsuarios}/>
-        <Stack.Screen name="MenuInasistencias" component={MenuInasistencias}/>
-        <Stack.Screen name="FormProfesores" component={FormProfesores}/>
-        <Stack.Screen name="FormUser" component={FormUser}/>
-        <Stack.Screen name="FormMateria" component={FormMateria}/>
-        <Stack.Screen name="FormCurso" component={FormCurso}/>
-        <Stack.Screen name="AltaRealizado" component={AltaRealizado}/>
-        <Stack.Screen name="ModRealizado" component={ModRealizado}/>
-        <Stack.Screen name="InstConfirmacion" component={InstConfirmacion}/>
-        <Stack.Screen name="InsRealizado" component={InsRealizado}/>
-
-      </Stack.Navigator>
-    </NavigationContainer>
+      <Navigation/>
     </NativeBaseProvider>
   );
 }
--- src/UI/nav/Navigation.tsx
+++ src/UI/nav/Navigation.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginForm from "../components/LoginForm";
+import MainMenu from "../components/MainMenu";
+import MenuProfesores from "../components/MenuProfesores";
+import MenuMaterias from "../components/MenuMaterias";
+import MenuCursos from "../components/MenuCursos";
+import MenuUsuarios from "../components/MenuUsuarios";
+import MenuInasistencias from "../components/MenuInasistencias";
+import FormProfesores from "../components/FormProfesores";
+import FormUser from "../components/FormUser";
+import FormMateria from "../components/FormMateria";
+import FormCurso from "../components/FormCurso";
+import AltaRealizado from "../components/AltaRealizado";
+import ModRealizado from "../components/ModRealizado";
+import InstConfirmacion from "../components/InstConfirmacion";
+
+const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
-    return (
-        
-    );
-}
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Inicio">
+        <Stack.Screen name="Inicio" component={LoginForm} />
+        <Stack.Screen name="MainMenu" component={MainMenu} />
+        <Stack.Screen name="MenuProfesores" component={MenuProfesores} />
+        <Stack.Screen name="MenuMaterias" component={MenuMaterias} />
+        <Stack.Screen name="MenuCursos" component={MenuCursos} />
+        <Stack.Screen name="MenuUsuarios" component={MenuUsuarios} />
+        <Stack.Screen name="MenuInasistencias" component={MenuInasistencias} />
+        <Stack.Screen name="FormProfesores" component={FormProfesores} />
+        <Stack.Screen name="FormUser" component={FormUser} />
+        <Stack.Screen name="FormMateria" component={FormMateria} />
+        <Stack.Screen name="FormCurso" component={FormCurso} />
+        <Stack.Screen name="AltaRealizado" component={AltaRealizado} />
+        <Stack.Screen name="ModRealizado" component={ModRealizado} />
+        <Stack.Screen name="InstConfirmacion" component={InstConfirmacion} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default Navigation;
