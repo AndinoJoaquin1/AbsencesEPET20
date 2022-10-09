b166194b9712b3298a02aed7f1146345b96f343d
Joaquin Andino <andino.joaquin.epet20@gmail.com>
2022-10-09 19:25:12 -0300

    refactor: App - Hook useWindowDimension

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { NativeBaseProvider } from 'native-base';
 import Navigation from './src/UI/nav/Navigation';
-
-
+import { useWindowDimensions } from 'react-native';
 //Ejecucion de componentes y navegacion  entre  pantallas
 
 
 export default function App() {
+  const { height, width } = useWindowDimensions();
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider width={{width}} height={{height}}>
       <Navigation/>
     </NativeBaseProvider>
-  );
+    )
 }
 
 
