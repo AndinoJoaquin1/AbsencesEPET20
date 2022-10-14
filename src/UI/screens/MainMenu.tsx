33fcfd7eec0028be9f55fd8281a6404296dc42b0
ianmoranf <82187893+ianmoranf@users.noreply.github.com>
2022-10-13 21:17:15 -0300

    Agregando_botones_mainmenu

--- src/UI/screens/MainMenu.tsx
+++ src/UI/screens/MainMenu.tsx
@@ -9,10 +9,29 @@ import Navigation from "../nav/Navigation";
 export  default function MainMenu ({navigation}:PropsNavigation) {
     return(
         <Center w="100%" flex={"1"}>
-            <Box safeArea w="90%">
-                <Button colorScheme="darkBlue" size="lg"
+            <Box safeArea w="90%"  alignItems="center" >
+
+                <Button colorScheme="orange" size="lg" margin="5"   
                 onPress={()=>navigation.navigate('MenuProfesores')}
-                >Inasistencia</Button>
+                >       Inasistencia       </Button>
+
+                   <Button colorScheme="pink" size="lg" margin="5"  
+                onPress={()=>navigation.navigate('MenuCursos')}
+                >          Cursos          </Button>
+
+                <Button colorScheme="blue" size="lg" margin="5"
+                onPress={()=>navigation.navigate('MenuInasistencias')}
+                >Lista De profesores</Button>
+
+                <Button colorScheme="yellow" size="lg" margin="5"
+                onPress={()=>navigation.navigate('MenuMaterias')}
+                >        Materias        </Button>
+            
+                <Button colorScheme="violet" size="lg" margin="5"
+                onPress={()=>navigation.navigate('MenuUsuario')}
+                >         Usuario         </Button>
+
+                 
             </Box>
         </Center>
     );
