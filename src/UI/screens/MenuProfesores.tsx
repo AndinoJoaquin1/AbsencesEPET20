64a7078de29d27886315911aaaef383da1e6a629
Joaquin Andino <andino.joaquin.epet20@gmail.com>
2022-10-23 23:55:57 -0300

    refactor: MenuProfesores - testeo de la base de datos

--- src/UI/screens/MenuProfesores.tsx
+++ src/UI/screens/MenuProfesores.tsx
@@ -2,24 +2,37 @@ import React from "react";
 import { FlatList, View } from "native-base";
 import { PropsNavigation } from "../interfaces/interfaces";
 import PressableProfesor from "../components/PressableProfesor";
+import { collection, onSnapshot, orderBy, query} from "firebase/firestore";
+import { database } from "../database/FirebaseConfig";
+
+
 
 const MenuProfesores = ({ navigation }: PropsNavigation) => {
-    const data = [
-      {
-        id: "1",
-        nombre: "Juan",
-        apellido: "Perez",
-        curso: "6to 1ra",
-        materia: "Seguridad Informatica",
-      }
-    ];
+  const [profesores, setProfesores] = React.useState([] as any);
+  React.useEffect(()=>{
+    const colletionRef = collection(database, 'prueba');
+    const q = query(colletionRef, orderBy('lastName','desc'));
+
+    const deconstructor = async()=> onSnapshot(q, querySnapshot => {
+      setProfesores(
+        querySnapshot.docs.map(doc => ({
+          id: doc.id,
+          firstName: doc.data().firstName,
+          lastName: doc.data().lastName,
+          materia: doc.data().materia,
+          curso: doc.data().curso
+        }))
+      );
+    })
+    return deconstructor;
+  }, [])
 
   return (
     <View flex="1">
       <FlatList
-        data={data}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <PressableProfesor apellido={item.apellido} nombre={item.nombre} curso={item.curso} materia={item.materia}/>}
+        data={profesores}
+        keyExtractor ={(item) => item.id}
+        renderItem={({ item }) => <PressableProfesor apellido={item.lastName} nombre={item.lastName} curso={item.curso} materia={item.materia}/>}
       />
     </View>
   );
