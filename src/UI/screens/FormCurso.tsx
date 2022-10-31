abfe03efb7ee2ce76dc2852b4ba0a090ace21b00
ianmoranf <82187893+ianmoranf@users.noreply.github.com>
2022-10-31 18:20:47 -0300

    Revert "Actualizando_formcurso"
    This reverts commit 8de02508a977f78feb8162904803c22170ed00e3.

--- src/UI/screens/FormCurso.tsx
+++ src/UI/screens/FormCurso.tsx
@@ -1,68 +1,50 @@
 import React from "react";
 import {View,Button,StyleSheet} from "react-native";
+import SelectDropdown from "react-native-select-dropdown";
 import {PropsNavigation} from '../interfaces/interfaces';
-import { Select , Box, Center ,CheckIcon } from "native-base";
+
 //componente de formulario de carga de cursos
 export default function FormCurso ({navigation}:PropsNavigation){
-   
-        const [service, setService] = React.useState("");
-        return(
+    const  Turno=["Mañana","Tarde","Vespertino"];
+    const Año=["Primero","Segundo","Tercero"];
+    const Division=[ "Primera" , "Segunda", "Tercera"];
+    return(
+            <View style={styles.container}>
+                <Button title="Back"
+                onPress={() => navigation.goBack()}/>
+                <Button title="Home"
+                onPress={() => navigation.navigate('MainMenu')}/>
+                
+                <SelectDropdown
+                data={Turno}
+                onSelect={(selectedItem, index)=>console.log(selectedItem,index)}
+                buttonTextAfterSelection={(selectedItem,index)=>{return selectedItem}}
+                defaultButtonText="Ingrese el turno"
+                />
+                
+                <SelectDropdown
+                data={Año}
+                onSelect={(selectedItem, index)=>console.log(selectedItem,index)}
+                buttonTextAfterSelection={(selectedItem,index)=>{return selectedItem}}
+                defaultButtonText="Ingrese el año"
+                />
 
-         <Center w="100%" flex={"1"} >
-            <Box   w="90%"    alignItems="center" >
-            <Button title="Back" 
-            onPress={() => navigation.goBack()}/>
+                   <SelectDropdown
+                data={Division}
+                onSelect={(selectedItem, index)=>console.log(selectedItem,index)}
+                buttonTextAfterSelection={(selectedItem,index)=>{return selectedItem}}
+                defaultButtonText="Ingrese  la Division"
+                />
+                <Button title="Agregar Curso"
+                onPress={() => navigation.navigate('AltaRealizado')}/>
+            </View>
+        );
+}
 
-
-
-              <Select selectedValue={service} minWidth="200" accessibilityLabel="Ingrese El Año" placeholder="Ingrese El Año" _selectedItem={{
-              bg: "teal.600",
-              endIcon: <CheckIcon size="5"   />
-            }} mt={10} onValueChange={itemValue => setService(itemValue)}>
-                <Select.Item label="Primero " value="ux" />
-                <Select.Item label="Segundo " value="ux" />
-                <Select.Item label="Tercero " value="ux" />
-                <Select.Item label="Cuarto " value="ux" />
-                <Select.Item label="Quinto " value="ux" />
-                <Select.Item label="Sexto " value="ux" />
-              </Select>
-            
-
-              <Select selectedValue={service} minWidth="200" accessibilityLabel="Ingrese La Divicion" placeholder="Ingrese La Divicion" _selectedItem={{
-              bg: "teal.600",
-              endIcon: <CheckIcon size="5" />
-            }} mt={10} onValueChange={itemValue => setService(itemValue)}>
-                <Select.Item label="Primera " value="ux" />
-                <Select.Item label="Segunda " value="ux" />
-                <Select.Item label="Tercera " value="ux" />
-                <Select.Item label="Cuarta " value="ux" />
-                <Select.Item label="Quinta " value="ux" />
-                <Select.Item label="Sexta" value="ux" />
-              </Select>
-            
-              <Select selectedValue={service} minWidth="200" accessibilityLabel="Ingrese El Turno " placeholder="Ingrese El Turno" _selectedItem={{
-              bg: "teal.600",
-              endIcon: <CheckIcon size="5" />
-            }} mt={10} onValueChange={itemValue => setService(itemValue)}>
-                <Select.Item label="Mañama " value="ux" />
-                <Select.Item label="Tarde " value="ux" />
-                <Select.Item label="Vespertino " value="ux" />
-              </Select>
-            
-
-
-            
-            </Box>
-          </Center>
-
-          )
-      };
-  
-    
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#abd',
+        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
