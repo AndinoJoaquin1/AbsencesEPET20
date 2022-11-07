4003941a581b241e4dba3bdef959cb6e6ac38ad0
Joaquin Andino <andino.joaquin.epet20@gmail.com>
2022-11-06 23:51:20 -0300

    refactor: PressableProfesor - refactorizacion para poder integrar el ModProfesor

--- src/UI/components/PressableProfesor.tsx
+++ src/UI/components/PressableProfesor.tsx
@@ -1,30 +1,22 @@
 import React from "react";
 import { AlertDialog, Box, Button, Heading, Pressable, Text } from "native-base";
-import RNDateTimePicker from "@react-native-community/datetimepicker";
+import { useNavigation } from "@react-navigation/native";
 
 interface Props{
-  nombre:string,
-  apellido:string,
+  id:string,
+  firstName:string,
+  lastName:string,
   materia:string,
   curso:string,
 }
 
-const PressableProfesor = ({nombre, apellido, curso, materia}:Props) => {
-  
-  
-  const [isOpen, setIsOpen] = React.useState(false);
-
-  const onClose = () => setIsOpen(false);
-  
-  const cancelRef = React.useRef(null);
-
-  return (
-    
+const PressableProfesor = ({id,firstName, lastName, curso, materia}:Props, {navigation}:PropsNavigation) => {
+  return (    
     <Box safeArea>
       {/*card*/}
       <Pressable
         marginX="5"
-        onPress={()=>setIsOpen(true)}
+  onPress={()=>navigation.navigate('ModProfesor',{id:id})}
       >
         {({ isPressed }) => { return(
           <Box bg={isPressed? "gray.300": "white"} p="5" shadow="8" rounded="10" m="1"
@@ -33,35 +25,13 @@ const PressableProfesor = ({nombre, apellido, curso, materia}:Props) => {
             
           }}
           >
-            <Heading fontSize="18">{nombre} {apellido}</Heading>
+            <Heading fontSize="18">{lastName} {firstName}</Heading>
             <Text mt="5">{materia}</Text>
             <Text>{curso}</Text>
           </Box>
         )
         }} 
       </Pressable>
-       {/*popup*/}
-      <AlertDialog safeArea leastDestructiveRef={cancelRef} isOpen={isOpen} onClose={onClose}>
-        <AlertDialog.Content>
-          <AlertDialog.CloseButton />
-          <AlertDialog.Header>Notificar Ausencia</AlertDialog.Header>
-          <AlertDialog.Body>
-            <Text>
-            Esta a punto de notificar a {nombre} como ausente. 
-            </Text>
-          </AlertDialog.Body>
-          <AlertDialog.Footer>
-            <Button.Group space={2}>
-              <Button variant="unstyled" colorScheme="coolGray" onPress={onClose} ref={cancelRef}>
-                Cancelar
-              </Button>
-              <Button colorScheme="danger" onPress={onClose}>
-                Continuar
-              </Button>
-            </Button.Group>
-          </AlertDialog.Footer>
-        </AlertDialog.Content>
-      </AlertDialog>
     </Box>
   );
 };
