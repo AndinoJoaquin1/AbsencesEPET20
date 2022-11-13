21a7fc34fa6a816a656feda139e1558fb15eba89
Joaquin Andino <andino.joaquin.epet20@gmail.com>
2022-11-13 18:19:11 -0300

    Feat: Registro - Verificacion del syntax del correo y contraseña

--- src/UI/screens/Registro.tsx
+++ src/UI/screens/Registro.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Box, Button, Center, Input } from "native-base";
+import React, { useState,useEffect } from "react";
+import { Box, Button, Center, FormControl, Input } from "native-base";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParams } from "../nav/Navigation";
 import { auth } from "../database/FirebaseConfig";
@@ -10,10 +10,29 @@ type PropsNavigation = NativeStackScreenProps<RootStackParams,'Registro'>
 // registro de ususario
 export default function Registo({ navigation }: PropsNavigation) {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  
-  const handleSingUp = () => {
-    createUserWithEmailAndPassword(auth,email, password)
+  const [firstPassword, setFirstPassword] = useState("");
+  const [secondPassword, setSecondPassword] = useState("");
+  const [formState, setFormState] = useState(false);
+
+  const emailSyntaxVerify = (value) =>{
+    const syntax = /^[a-zA-Z0-9_.-]+@gmail+.com/;
+    if (!syntax.test(value)){
+      console.log('ta male')
+    }else{
+      console.log("ta biene")
+    }
+}
+  const passwordSyntaxVerify = (value) => {
+    const syntax = /^[A-Za-z0-9]+$/;
+    if (syntax.test(value)&&value.length>=8&&value.length<=16){
+      console.log("ta bienp")
+    }else{
+      console.log("ta malp")
+    }
+  }
+
+  const handleSingUp = async () => {
+      await createUserWithEmailAndPassword(auth,email, firstPassword)
       .then((userCredentials) => {
         const user = userCredentials.user;
         console.log(user.email);
@@ -21,16 +40,27 @@ export default function Registo({ navigation }: PropsNavigation) {
       .catch(e=>alert(e.message))
     }
 
+    
+
   return (
     <Center safeArea>
       <Box mt="25%" width="90%">
-        <Input 
-        placeholder="Mail" onChangeText={(value) => setEmail(value)} />
-        <Input
-        placeholder="Contraseña" onChangeText={(value) => setPassword(value)}
-        />
-        <Button onPress={handleSingUp}>Agregar</Button>
+        <Input placeholder="Mail" onChangeText={(value) => emailSyntaxVerify(value)} />
+        <FormControl isInvalid={formState}>
+        <Input placeholder="Contraseña" onChangeText={(value) => passwordSyntaxVerify(value)}/>
+        <Input placeholder="Repetir Contraseña" onChangeText={(value) => setSecondPassword(value)}/>
+        <FormControl.ErrorMessage>
+          Error
+        </FormControl.ErrorMessage>
+        </FormControl>
+        <Button 
+        
+        isDisabled={formState}
+        mt="5"
+        >Agregar
+        </Button>
       </Box>
     </Center>
   );
 }
+
