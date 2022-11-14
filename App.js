bccb4008824f841853d768479451d856dd7cbf77
Joaquin Andino <andino.joaquin.epet20@gmail.com>
2022-11-14 00:47:48 -0300

    Refactor: Navigation - Adaptando Navigation con AuthContext

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { NativeBaseProvider } from 'native-base';
 import Navigation from './src/UI/nav/Navigation';
 import { useWindowDimensions } from 'react-native';
-import { AuthProvider } from './src/UI/context/useContext';
+import { authContext, AuthProvider } from './src/UI/context/AuthContext';
 //Ejecucion de componentes y navegacion  entre  pantallas
 
 
 export default function App() {
   const { height, width } = useWindowDimensions();
   return (
-    <AuthProvider>
+    <AuthProvider value={authContext}>
 
     <NativeBaseProvider width={{width}} height={{height}}>
       <Navigation/>
--- src/UI/nav/Navigation.tsx
+++ src/UI/nav/Navigation.tsx
@@ -16,67 +16,91 @@ import AltaRealizado from "../screens/AltaRealizado";
 import ModRealizado from "../screens/ModRealizado";
 import InstConfirmacion from "../screens/InstConfirmacion";
 import ModProfesor from "../screens/ModProfesor";
-import Registo from "../screens/Registro";
+import Registro from "../screens/Registro";
+import { useAuth } from "../context/AuthContext";
+import SplashScreen from "../screens/SplashScreen";
 
 export type RootStackParams = {
-Inicio,
-MainMenu,
-MenuProfesores,
-MenuMaterias,
-MenuCursos,
-MenuUsuarios,
-MenuInasistencias,
-FormProfesores,
-FormUser,
-FormMateria,
-FormCurso,
-AltaRealizado,
-ModRealizado,
-InstConfirmacion
-ModProfesor:{
-  id:string,
-  lastName:string,
-  firstName:string
-},
-Registro
-}
+  Inicio;
+  MainMenu;
+  MenuProfesores;
+  MenuMaterias;
+  MenuCursos;
+  MenuUsuarios;
+  MenuInasistencias;
+  FormProfesores;
+  FormUser;
+  FormMateria;
+  FormCurso;
+  AltaRealizado;
+  ModRealizado;
+  InstConfirmacion;
+  ModProfesor: {
+    id: string;
+    lastName: string;
+    firstName: string;
+  };
+  Registro;
+  SplashScreen;
+};
 const Stack = createNativeStackNavigator<RootStackParams>();
 
-
 const Navigation = () => {
+  const { user, loading } = useAuth();
   return (
-    <NavigationContainer>
-      <Stack.Navigator 
-      initialRouteName="Inicio" 
-      screenOptions={{
-        headerTitleAlign:"center",
-        headerStyle:{backgroundColor:'#005db4'},
-        headerTintColor:'#ffffff',
-        headerTitleStyle:{fontFamily:'sans-serif-light'}
-        }}>
-        <Stack.Screen 
-        name="MainMenu" 
-        component={MainMenu} 
-        options={{
-          headerBackVisible:false,
-          title:"Absences EPET20"
-        }} 
-        />
-        <Stack.Screen name="Inicio" options={{headerShown:false}} component={LoginForm} />
-        <Stack.Screen name="MenuProfesores" component={MenuProfesores} />
-        <Stack.Screen name="MenuMaterias" component={MenuMaterias} />
-        <Stack.Screen name="MenuCursos" component={MenuCursos} />
-        <Stack.Screen name="MenuUsuarios" component={MenuUsuarios} />
-        <Stack.Screen name="MenuInasistencias" component={MenuInasistencias} />
-        <Stack.Screen name="FormProfesores" component={FormProfesores} />
-        <Stack.Screen name="FormUser" component={FormUser} />
-        <Stack.Screen name="FormMateria" component={FormMateria} />
-        <Stack.Screen name="FormCurso" component={FormCurso} />
-        <Stack.Screen name="AltaRealizado" component={AltaRealizado} />
-        <Stack.Screen name="ModRealizado" component={ModRealizado} />
-        <Stack.Screen name="InstConfirmacion" component={InstConfirmacion} />
-        <Stack.Screen name="ModProfesor" component={ModProfesor}/>
-        <Stack.Screen name="Registro" component={Registo}/>
+    <NavigationContainer >
+      <Stack.Navigator
+        screenOptions={{
+          headerTitleAlign: "center",
+          headerStyle: { backgroundColor: "#005db4" },
+          headerTintColor: "#ffffff",
+          headerTitleStyle: { fontFamily: "sans-serif-light" },
+        }}
+      >
+        {loading?(
+          <Stack.Screen name="SplashScreen" component={SplashScreen}/>
+          ):null }
+        
+        {user == null ? (
+          <>
+            <Stack.Screen
+              name="Inicio"
+              options={{ headerShown: false }}
+              component={LoginForm}
+            />
+            <Stack.Screen name="Registro" component={Registro} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen
+              name="MainMenu"
+              component={MainMenu}
+              options={{
+                headerBackVisible: false,
+                title: "Absences EPET20",
+              }}
+              />
+            <Stack.Screen name="MenuProfesores" component={MenuProfesores} />
+            <Stack.Screen name="MenuMaterias" component={MenuMaterias} />
+            <Stack.Screen name="MenuCursos" component={MenuCursos} />
+            <Stack.Screen name="MenuUsuarios" component={MenuUsuarios} />
+            <Stack.Screen
+              name="MenuInasistencias"
+              component={MenuInasistencias}
+              />
+            <Stack.Screen name="FormProfesores" component={FormProfesores} />
+            <Stack.Screen name="FormUser" component={FormUser} />
+            <Stack.Screen name="FormMateria" component={FormMateria} />
+            <Stack.Screen name="FormCurso" component={FormCurso} />
+            <Stack.Screen name="AltaRealizado" component={AltaRealizado} />
+            <Stack.Screen name="ModRealizado" component={ModRealizado} />
+            <Stack.Screen
+              name="InstConfirmacion"
+              component={InstConfirmacion}
+              />
+            <Stack.Screen name="ModProfesor" component={ModProfesor} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
