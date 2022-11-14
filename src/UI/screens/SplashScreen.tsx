bd9d0e4df94a58c80e2dde9031a40067f7f873dd
Joaquin Andino <andino.joaquin.epet20@gmail.com>
2022-11-14 00:47:14 -0300

    Feat: SplashScreen - Creando Pantalla de carga

---
+++ src/UI/screens/SplashScreen.tsx
@@ -0,0 +1,12 @@
+import { Center, Text } from 'native-base';
+import React from 'react';
+
+const SplashScreen = () => {
+    return (
+        <Center>
+            <Text>Cargando</Text>
+        </Center>
+    );
+}
+
+export default SplashScreen;
