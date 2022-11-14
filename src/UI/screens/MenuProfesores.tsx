5c3d7d03e9ecb30cd0a2ac4284840a4b9bd1dc21
Joaquin Andino <andino.joaquin.epet20@gmail.com>
2022-11-14 16:49:54 -0300

    Refactor: MenuProfesores - Adaptandolo para el uso de useFirebaseFunctions

--- src/UI/screens/MenuProfesores.tsx
+++ src/UI/screens/MenuProfesores.tsx
@@ -1,46 +1,46 @@
 import React from "react";
 import { FlatList, View } from "native-base";
 import PressableProfesor from "../components/PressableProfesor";
-import { collection, onSnapshot, orderBy, query} from "firebase/firestore";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { database } from "../database/FirebaseConfig";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParams } from "../nav/Navigation";
+import { getProfesores } from "../hooks/useFirebaseFunction";
 
-type Props = NativeStackScreenProps<RootStackParams,'MenuProfesores'>
+type PropsNavigation = NativeStackScreenProps<
+  RootStackParams,
+  "MenuProfesores"
+>;
 
-const MenuProfesores = ({ navigation }: Props) => {
+const MenuProfesores = ({ navigation }: PropsNavigation) => {
+  const [profesores, setProfesores] = React.useState(new Array());
 
-  const [profesores, setProfesores] = React.useState([] as any);
-  
-  const colletionRef = collection(database, 'prueba');
-  
-  const q = query(colletionRef, orderBy('lastName','desc'));
- 
-  const consultaDB = ()=> onSnapshot(q, querySnapshot => {
-    setProfesores ( 
-      querySnapshot.docs.map(doc => ({
-        id: doc.id,
-        firstName: doc.data().firstName,
-        lastName: doc.data().lastName,
-        materia: doc.data().materia,
-        curso: doc.data().curso
-      }))
-    );
-  })
+  const consultaDB = async () => {
+    const getData = await getProfesores();
+    setProfesores(getData.profesores);
+  };
 
-  React.useEffect(()=>{
-    const fecthProfesores= async()=>{
-      const data = await consultaDB();
-    }
-    fecthProfesores();
-  }, [])
+  React.useEffect(() => {
+    consultaDB();
+  }, []);
 
   return (
     <View flex="1">
       <FlatList
         data={profesores}
-        keyExtractor ={(item) => item.id}
-        renderItem={({ item }) => <PressableProfesor lastName={item.lastName} firstName={item.lastName} curso={item.curso} materia={item.materia} id={item.id} onPress={id=>{navigation.navigate('ModProfesor',{id})}} />}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <PressableProfesor
+            lastName={item.lastName}
+            firstName={item.lastName}
+            curso={item.curso}
+            materia={item.materia}
+            id={item.id}
+            onPress={(id, lastName, firstName) => {
+              navigation.navigate("ModProfesor", { id, lastName, firstName });
+            }}
+          />
+        )}
       />
     </View>
   );
