1617124c8ffc444556b75a18d5e949c1d150b258
Joaquin Andino <andino.joaquin.epet20@gmail.com>
2022-11-14 18:29:10 -0300

    Refactor: useFirebaseFunction

--- src/UI/hooks/useFirebaseFunctions.tsx
+++ src/UI/hooks/useFirebaseFunctions.tsx
@@ -1,15 +1,26 @@
-import { collection, getDocs, limit, onSnapshot, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { database } from "../database/FirebaseConfig";
 
-export const getProfesores = async() =>{
-    const profesores = new Array();
-    const colletionRef = collection(database, 'prueba');
-    const q = await getDocs(colletionRef,orderBy('lastName'),limit(10));
-    q.forEach((doc)=>{
-        let profData = doc.data();
-        profData.id= doc.id;
-        profesores.push(profData);
-        console.log(profesores);
-    });
-    return {profesores};
-}
+export const getProfesores = async () => {
+  const profesores = new Array();
+  const q = query(
+    collection(database, "prueba"),
+    orderBy("lastName"),
+    limit(10)
+  );
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    let profData = doc.data();
+    profData.id = doc.id;
+    profesores.push(profData);
+    console.log(profesores);
+  });
+  return { profesores };
+};
