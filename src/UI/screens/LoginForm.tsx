5f140cc9ab07ced423a946844666d6f4a6447acc
Joaquin Andino <andino.joaquin.epet20@gmail.com>
2022-11-14 00:46:20 -0300

    Refactor: LoginForm - Adaptando LoginForm con AuthContext

--- src/UI/screens/LoginForm.tsx
+++ src/UI/screens/LoginForm.tsx
@@ -1,10 +1,26 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Box, Button, Center, FormControl, Heading,  Input } from "native-base";
-import React from "react";
-import { PropsNavigation } from "../interfaces/interfaces";
+import React,{useState} from "react";
+import { useAuth } from "../context/AuthContext";
+import { RootStackParams } from "../nav/Navigation";
+
+type PropsNavigation = NativeStackScreenProps<RootStackParams,'Inicio'>
 
 // Componente de login
 
 export default function LoginForm({ navigation }: PropsNavigation) {
+  const {login} = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    try {
+      await login(email, password);
+      navigation.navigate('MainMenu')
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
   return (
     <Center flex={1}>
       <Box safeArea p="2" py="8" w="90%" maxW="290">
@@ -13,16 +29,21 @@ export default function LoginForm({ navigation }: PropsNavigation) {
         </Heading>
           <FormControl mt="25">
             <FormControl.Label>Usuario</FormControl.Label>
-            <Input />
+            <Input onChangeText={(value)=>setEmail(value)}/>
           </FormControl>
           <FormControl>
             <FormControl.Label>Contraseña</FormControl.Label>
-            <Input type="password" />
+            <Input type="password" onChangeText={(value)=>setPassword(value)}/>
           </FormControl>
-          <Button mt="2"  marginTop="12" colorScheme="darkBlue"
-          onPress={()=> navigation.navigate('MainMenu')}
+          <Button mt="5"  colorScheme="darkBlue"
+          onPress={handleSubmit}
           >
             Ingresar
+          </Button>
+          <Button mt="5" colorScheme="blueGray"
+          onPress={()=>navigation.navigate('Registro')}
+          >
+            Registrate
           </Button>
       </Box>
     </Center>
