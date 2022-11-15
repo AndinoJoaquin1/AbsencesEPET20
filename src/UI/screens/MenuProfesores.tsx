9565c21c8b941b95650192838c96aa2f3e56bc9e
Joaquin Andino <andino.joaquin.epet20@gmail.com>
2022-11-15 00:33:10 -0300

    Feat: MenuProfesores - Ahora carga de a pocos docs

--- src/UI/screens/MenuProfesores.tsx
+++ src/UI/screens/MenuProfesores.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { FlatList, View } from "native-base";
+import { Center, FlatList, Input, Spinner, View } from "native-base";
 import PressableProfesor from "../components/PressableProfesor";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { database } from "../database/FirebaseConfig";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParams } from "../nav/Navigation";
-import { getProfesores } from "../hooks/useFirebaseFunction";
+import { getMoreProfesores, getProfesores } from "../hooks/useFirebaseFunctions";
 
 type PropsNavigation = NativeStackScreenProps<
   RootStackParams,
@@ -14,35 +14,58 @@ type PropsNavigation = NativeStackScreenProps<
 
 const MenuProfesores = ({ navigation }: PropsNavigation) => {
   const [profesores, setProfesores] = React.useState(new Array());
+  const [lastDoc, setLastDoc] = React.useState(Object);
+  const [lastDocIsLoad, setLastDocIsLoad] = React.useState(false);
 
-  const consultaDB = async () => {
+  const consulta = async () => {
     const getData = await getProfesores();
-    setProfesores(getData.profesores);
+    setProfesores([...profesores, ...getData.profesores]);
+    setLastDoc(getData.lastVisible);
+    console.log(lastDoc)
   };
 
+  const moreData = async() => {
+    if(!lastDocIsLoad){
+
+      const  getData = await getMoreProfesores(lastDoc)
+      setProfesores([...profesores, ...getData.profesores]);
+      setLastDoc(getData.lastVisible);
+      getData.profesores.length==0?setLastDocIsLoad(true):setLastDocIsLoad(false);
+    }
+  }
+
   React.useEffect(() => {
-    consultaDB();
+    consulta();
   }, []);
 
   return (
-    <View flex="1">
-      <FlatList
-        data={profesores}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <PressableProfesor
-            lastName={item.lastName}
-            firstName={item.lastName}
-            curso={item.curso}
-            materia={item.materia}
-            id={item.id}
-            onPress={(id, lastName, firstName) => {
-              navigation.navigate("ModProfesor", { id, lastName, firstName });
-            }}
-          />
-        )}
-      />
-    </View>
+    <Center flex="1">
+      <Input placeholder="Filtar por Nombre" size="md" w="90%" mt="1" shadow="1" />
+      
+        <FlatList w="full"
+        onEndReached={moreData}
+        ListFooterComponent={!lastDocIsLoad&&<Spinner mt="1" size="lg"/>}
+          data={profesores}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <PressableProfesor
+              lastName={item.lastName}
+              firstName={item.firstName}
+              curso={item.curso}
+              materia={item.materia}
+              id={item.id}
+              onPress={(id, lastName, firstName,) => {
+                navigation.navigate("ModProfesor", {
+                  id,
+                  lastName,
+                  firstName
+                });
+              }}
+            />
+          )}
+        />
+    
+    </Center>
   );
 };
 
