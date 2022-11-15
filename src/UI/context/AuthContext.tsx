afd2ffe96ee9372eb1438962e633d67192737b4e
Joaquin Andino <andino.joaquin.epet20@gmail.com>
2022-11-15 01:14:36 -0300

    Feat: AutContext - Manda mensajes de error del backend

--- src/UI/context/AuthContext.tsx
+++ src/UI/context/AuthContext.tsx
@@ -1,45 +1,76 @@
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { createContext, useContext, useEffect, useState } from "react";
 import { auth } from "../database/FirebaseConfig";
 
 export const authContext = createContext(null);
 
-export const useAuth = () =>{
-    const context = useContext(authContext);
-    return context
-} 
+export const useAuth = () => {
+  const context = useContext(authContext);
+  return context;
+};
 
-export const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [messageError, setMessageError] = useState("");
 
-    const signUp = (email, password) => {
-        return createUserWithEmailAndPassword(auth, email, password);
-    };
+  const signUp = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-    const login = (email, password) => {
-        return signInWithEmailAndPassword(auth, email, password);
-    };
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-    const logOut = () => signOut(auth);
+  const logOut = () => signOut(auth);
 
+  const getError = (error) => {
+    switch (error.code) {
+      case "auth/invalid-email":
+        setMessageError("Correo electronico invalido");
+        break;
+      case "auth/weak-password":
+        setMessageError("Contraseña muy corta, debe de ser de por al menos 7 caracteres");
+        break;
+      case "auth/wrong-password":
+        setMessageError("Contraseña incorrecta");
+        break;
+      case "auth/email-already-in-use":
+        setMessageError("Email en uso");
+        break;
+      default:
+        setMessageError("Error desconocido");
+        break;
+    }
+    return messageError;
+  };
 
-    useEffect(() => {
-        const observer = onAuthStateChanged(auth, (currentUser) => {
-          console.log({ currentUser });
-          setUser(currentUser);
-          setLoading(false);
-        });
-        return () => observer();
-      }, []);
+  useEffect(() => {
+    const observer = onAuthStateChanged(auth, (currentUser) => {
+      console.log({ currentUser });
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => observer();
+  }, []);
 
-    return (<authContext.Provider value={{
-    login,
-    logOut,
-    signUp,
-    user,
-    loading
-    }}>
-        {children}
-    </authContext.Provider>);
-}
+  return (
+    <authContext.Provider
+      value={{
+        login,
+        logOut,
+        signUp,
+        user,
+        loading,
+        getError,
+      }}
+    >
+      {children}
+    </authContext.Provider>
+  );
+};
--- src/UI/screens/LoginForm.tsx
+++ src/UI/screens/LoginForm.tsx
@@ -1,5 +1,5 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Box, Button, Center, FormControl, Heading,  Input } from "native-base";
+import { Box, Button, Center, FormControl, Heading,  Input, Text } from "native-base";
 import React,{useState} from "react";
 import { useAuth } from "../context/AuthContext";
 import { RootStackParams } from "../nav/Navigation";
@@ -9,16 +9,18 @@ type PropsNavigation = NativeStackScreenProps<RootStackParams,'Inicio'>
 // Componente de login
 
 export default function LoginForm({ navigation }: PropsNavigation) {
-  const {login} = useAuth();
+  const {login,getError} = useAuth();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [displayError, setDisplayError] = useState("");
+  
   const handleSubmit = async (e) => {
     try {
+      setDisplayError("");
       await login(email, password);
       navigation.navigate('MainMenu')
     } catch (e) {
-      console.log(e.message);
+      setDisplayError(getError(e));
     }
   };
   return (
@@ -35,6 +37,7 @@ export default function LoginForm({ navigation }: PropsNavigation) {
             <FormControl.Label>Contraseña</FormControl.Label>
             <Input type="password" onChangeText={(value)=>setPassword(value)}/>
           </FormControl>
+          <Text mt="1" color="red">{displayError}</Text>
           <Button mt="5"  colorScheme="darkBlue"
           onPress={handleSubmit}
           >
