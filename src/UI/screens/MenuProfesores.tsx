e5a80ff6813e9a56d2322b72cc596bf460fc70cb
Joaquin Andino <andino.joaquin.epet20@gmail.com>
2022-11-15 00:35:22 -0300

    no se que paso pero se subio mal lmao

--- src/UI/screens/MenuProfesores.tsx
+++ src/UI/screens/MenuProfesores.tsx
@@ -5,7 +5,10 @@ import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { database } from "../database/FirebaseConfig";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParams } from "../nav/Navigation";
-import { getMoreProfesores, getProfesores } from "../hooks/useFirebaseFunctions";
+import {
+  getMoreProfesores,
+  getProfesores,
+} from "../hooks/useFirebaseFunctions";
 
 type PropsNavigation = NativeStackScreenProps<
   RootStackParams,
@@ -21,18 +24,19 @@ const MenuProfesores = ({ navigation }: PropsNavigation) => {
     const getData = await getProfesores();
     setProfesores([...profesores, ...getData.profesores]);
     setLastDoc(getData.lastVisible);
-    console.log(lastDoc)
+    console.log(lastDoc);
   };
 
-  const moreData = async() => {
-    if(!lastDocIsLoad){
-
-      const  getData = await getMoreProfesores(lastDoc)
+  const moreData = async () => {
+    if (!lastDocIsLoad) {
+      const getData = await getMoreProfesores(lastDoc);
       setProfesores([...profesores, ...getData.profesores]);
       setLastDoc(getData.lastVisible);
-      getData.profesores.length==0?setLastDocIsLoad(true):setLastDocIsLoad(false);
+      getData.profesores.length == 0
+        ? setLastDocIsLoad(true)
+        : setLastDocIsLoad(false);
     }
-  }
+  };
 
   React.useEffect(() => {
     consulta();
@@ -40,31 +44,37 @@ const MenuProfesores = ({ navigation }: PropsNavigation) => {
 
   return (
     <Center flex="1">
-      <Input placeholder="Filtar por Nombre" size="md" w="90%" mt="1" shadow="1" />
-      
-        <FlatList w="full"
+      <Input
+        placeholder="Filtar por Nombre"
+        size="md"
+        w="90%"
+        mt="1"
+        shadow="1"
+      />
+
+      <FlatList
+        w="full"
         onEndReached={moreData}
-        ListFooterComponent={!lastDocIsLoad&&<Spinner mt="1" size="lg"/>}
-          data={profesores}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <PressableProfesor
-              lastName={item.lastName}
-              firstName={item.firstName}
-              curso={item.curso}
-              materia={item.materia}
-              id={item.id}
-              onPress={(id, lastName, firstName,) => {
-                navigation.navigate("ModProfesor", {
-                  id,
-                  lastName,
-                  firstName
-                });
-              }}
-            />
-          )}
-        />
-    
+        ListFooterComponent={!lastDocIsLoad && <Spinner mt="1" size="lg" />}
+        data={profesores}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <PressableProfesor
+            lastName={item.lastName}
+            firstName={item.firstName}
+            curso={item.curso}
+            materia={item.materia}
+            id={item.id}
+            onPress={(id, lastName, firstName) => {
+              navigation.navigate("ModProfesor", {
+                id,
+                lastName,
+                firstName,
+              });
+            }}
+          />
+        )}
+      />
     </Center>
   );
 };
