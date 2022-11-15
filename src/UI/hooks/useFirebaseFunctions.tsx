4d383a0b080ced5a51f2fadaee4a68564a47f1ad
Joaquin Andino <andino.joaquin.epet20@gmail.com>
2022-11-15 00:32:39 -0300

    Feat: useFirebaseFunctions - Creando getMoreProfesores

--- src/UI/hooks/useFirebaseFunctions.tsx
+++ src/UI/hooks/useFirebaseFunctions.tsx
@@ -1,10 +1,13 @@
 import {
   collection,
+  doc,
+  getDoc,
   getDocs,
   limit,
   onSnapshot,
   orderBy,
   query,
+  startAfter,
 } from "firebase/firestore";
 import { database } from "../database/FirebaseConfig";
 
@@ -16,11 +19,31 @@ export const getProfesores = async () => {
     limit(10)
   );
   const querySnapshot = await getDocs(q);
+  const lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1];
   querySnapshot.forEach((doc) => {
     let profData = doc.data();
     profData.id = doc.id;
     profesores.push(profData);
     console.log(profesores);
   });
-  return { profesores };
+  return { profesores, lastVisible };
+};
+
+export const getMoreProfesores = async (lastDoc) => {
+  const profesores = new Array();
+  const q = query(
+    collection(database, "prueba"),
+    orderBy("lastName"),
+    startAfter(lastDoc),
+    limit(10)
+  );
+  const querySnapshot = await getDocs(q);
+  const lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1];
+  querySnapshot.forEach((doc) => {
+    let profData = doc.data();
+    profData.id = doc.id;
+    profesores.push(profData);
+    console.log(profesores);
+  });
+  return { profesores, lastVisible };
 };
